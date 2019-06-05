880a625baed3b6829769098202842e7c833f7c78
Anastasiia <nastia4019@gmail.com>
2019-06-05 22:01:11 +0300

    Updates

---
+++ precache-manifest.f3653d702460a7884c72614601bef90f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e9e37cf071c67491f2f91b516641912",
+    "url": "/React/static/media/back.8e9e37cf.png"
+  },
+  {
+    "revision": "f07b1af538ceffa8e6c5a18e9ab46356",
+    "url": "/React/static/media/Loading_C-512.f07b1af5.png"
+  },
+  {
+    "revision": "61a9096e1396fb9c7f21",
+    "url": "/React/static/js/runtime~main.61a9096e.js"
+  },
+  {
+    "revision": "1746c45ab005a599c187",
+    "url": "/React/static/js/main.1746c45a.chunk.js"
+  },
+  {
+    "revision": "c8c3aad7c721efe75798",
+    "url": "/React/static/js/2.c8c3aad7.chunk.js"
+  },
+  {
+    "revision": "1746c45ab005a599c187",
+    "url": "/React/static/css/main.aeb0f24b.chunk.css"
+  },
+  {
+    "revision": "176f856edc9da3b8447b590a29895d5e",
+    "url": "/React/index.html"
+  }
+];
